127baef8cad836ccce205e53b236dff6cc53e3ae
Jarett Sisk <jarett.sisk@gmail.com>
2019-07-27 23:13:21 -0700

    error testing

--- src/routes/user.js
+++ src/routes/user.js
@@ -61,6 +61,9 @@ router.post("/users/login", async (req, res) => {
   const login = async () => {
     try {
       const user = await User.findByCredentials(req.body.email, req.body.password);
+      if (!user) {
+        return res.status(500).send(error.message);
+      }
       const token = await user.generateAuthToken();
       res.send({ user, token});
       return;
