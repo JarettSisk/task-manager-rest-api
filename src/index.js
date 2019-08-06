0c34b1e66433ced277a6a24a0cecb9b7971c1585
Jarett Sisk <jarett.sisk@gmail.com>
2019-08-05 20:45:48 -0700

    making changes to cors

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const port = process.env.PORT;
 app.use(cors({
-  origin: "http://127.0.0.1:8080/"
+  origin: "http://127.0.0.1:8080"
 }));
 
 // Parse the incoming JSON
